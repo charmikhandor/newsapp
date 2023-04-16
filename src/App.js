c0c3118875194cabcbd6629cbf612a5ec9ba48df
charmikhandor <102253770+charmikhandor@users.noreply.github.com>
2023-04-16 09:05:56 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,113 +1,112 @@
 import Navbar from "./components/Navbar";
 import News from "./components/News";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
-const App =()=>{
-  
-  const [progress, setProgress] = useState(0)
+const App = () => {
+  const [progress, setProgress] = useState(0);
 
-    return (
-      <div>
-        <Router>
-          <Navbar />
-          <LoadingBar color="#f11946" progress={this.state.progress} />
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="general"
-                  country="in"
-                  pageSize={8}
-                  category="general"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/business"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="business"
-                  country="in"
-                  pageSize={8}
-                  category="business"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/sports"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="sports"
-                  country="in"
-                  pageSize={8}
-                  category="sports"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/entertainment"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="entertainment"
-                  country="in"
-                  pageSize={8}
-                  category="entertainment"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/health"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="health"
-                  country="in"
-                  pageSize={8}
-                  category="health"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/science"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="science"
-                  country="in"
-                  pageSize={8}
-                  category="science"
-                />
-              }
-            />
-            <Route
-              exact
-              path="/technology"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="technology"
-                  country="in"
-                  pageSize={8}
-                  category="technology"
-                />
-              }
-            />
-          </Routes>
-        </Router>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <LoadingBar color="#f11946" progress={progress} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                country="in"
+                pageSize={8}
+                category="general"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                setProgress={setProgress}
+                key="business"
+                country="in"
+                pageSize={8}
+                category="business"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                setProgress={setProgress}
+                key="sports"
+                country="in"
+                pageSize={8}
+                category="sports"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                setProgress={setProgress}
+                key="entertainment"
+                country="in"
+                pageSize={8}
+                category="entertainment"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                setProgress={setProgress}
+                key="health"
+                country="in"
+                pageSize={8}
+                category="health"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                setProgress={setProgress}
+                key="science"
+                country="in"
+                pageSize={8}
+                category="science"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                setProgress={setProgress}
+                key="technology"
+                country="in"
+                pageSize={8}
+                category="technology"
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-  export default App;
+export default App;
